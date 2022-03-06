3c089a8414429c19db522f72884ac54d8d7391f6
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-03-06 23:11:32 +0900

    create: Hello.tsx This is test.file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Hello from "./Hello";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <Hello />
       </header>
     </div>
   );
